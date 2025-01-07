2e9708bad9aedc4d54a3d3a221beace52279465a
Raveesh Kumar <92170670+Raveesh1007@users.noreply.github.com>
2025-01-07 11:51:16 +0530

    3rd commit

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,57 @@
-use tungstenite::{connect, Message};
-use url::Url;
+use async_tungstenite::tokio::connect_async;
+use async_tungstenite::tungstenite::Message;
+use futures_util::sink::SinkExt;  // For send
+use futures_util::stream::StreamExt;  // For next
+use tokio::time::{sleep, Duration};
+use tokio::runtime::Runtime;
 
-fn main(){
-    let url = "wss://rpc.polkadot.io";
+fn main() {
+    let rt = Runtime::new().unwrap();
+    rt.block_on(async {
+        let url = "wss://rpc.polkadot.io";
 
-    let (mut socket, _response) = connect(Url::parse(url).unwrap()).expect("Can't Connect");
+        // Attempt to connect to the Polkadot WebSocket endpoint
+        match connect_async(url).await {
+            Ok((mut ws_stream, _response)) => {
+                println!("Connected to Polkadot node!");
 
-    println!("Connected to the polkadot node:");
+                // JSON-RPC request to subscribe to new block headers
+                let request = r#"{
+                    "jsonrpc": "2.0",
+                    "method": "chain_subscribeNewHeads",
+                    "params": [],
+                    "id": 1
+                }"#;
 
-    let request = r#"
-        {
-            "jsonrpc": "2.0",
-            "method": "chain_subscribeNewHeads",
-            "params": [],
-            "id": 1
+                // Send subscription request
+                if let Err(e) = ws_stream.send(Message::Text(request.into())).await {
+                    eprintln!("Failed to send subscription request: {:?}", e);
+                    return;
+                }
+
+                println!("Subscribed to new heads.");
+
+                // Listen for incoming messages
+                while let Some(msg) = ws_stream.next().await {
+                    match msg {
+                        Ok(message) => {
+                            if let Message::Text(text) = message {
+                                println!("Received: {}", text);
+                            } else {
+                                println!("Received non-text message: {:?}", message);
+                            }
+                        }
+                        Err(e) => {
+                            eprintln!("Error reading message: {:?}", e);
+                            sleep(Duration::from_secs(1)).await; // Retry delay
+                            break;
+                        }
+                    }
+                }
+            }
+            Err(e) => {
+                eprintln!("Failed to connect to Polkadot node: {:?}", e);
+            }
         }
-    "#;
-    socket.write_message(Message::Text(request.into())).unwrap();
-
-    loop{
-        let msg = socket.read_message().expect("Error reading message");
-        println!("Received: {:?}", msg);
-    }
+    });
 }
